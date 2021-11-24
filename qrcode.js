a0b2c7d7455da9046836352f871095fc3fead745
sunrui <icarux@126.com>
2021-11-24 16:23:22 +0800

    add codewords count table

--- qrcode.js
+++ qrcode.js
@@ -45,7 +45,7 @@ var QRCode;
     var QRErrorCorrectionLevel = {L : 1, M : 0, Q : 3, H : 2};
     var QRErrorCorrectionLevelIndex = [1, 0, 3, 2]; // <-[QRErrorCorrectionLevel.M] == 1
     var QRMaskPattern = {PATTERN000 : 0, PATTERN001 : 1, PATTERN010 : 2, PATTERN011 : 3, PATTERN100 : 4, PATTERN101 : 5, PATTERN110 : 6, PATTERN111 : 7};
-    var QRCodeLimitLength = [[],        // version as row, error correction level(L, M, Q, H) as col
+    var QRDataCapacity = [[],        // version as row, error correction level(L, M, Q, H) as col
                             [17, 14, 11, 7],
                             [32, 26, 20, 14],
                             [53, 42, 32, 24],
@@ -87,12 +87,54 @@ var QRCode;
                             [2809, 2213, 1579, 1219],
                             [2953, 2331, 1663, 1273]
                         ];
+    var QRCodewordsCount = [[],      // Number of data codewords. version as row, error correction level(L, M, Q, H) as col
+            [19, 16, 13, 9],
+            [34, 28, 22, 16],
+            [55, 44, 34, 26],
+            [80, 64, 48, 36],
+            [108, 86, 62, 46],
+            [136, 108, 76, 60],
+            [156, 124, 88, 66],
+            [194, 154, 110, 86],
+            [232, 182, 132, 100],
+            [274, 216, 154, 122],
+            [324, 254, 180, 140],
+            [370, 290, 206, 158],
+            [428, 334, 244, 180],
+            [461, 365, 261, 197],
+            [523, 415, 295, 223],
+            [589, 453, 325, 253],
+            [647, 507, 367, 283],
+            [721, 563, 397, 313],
+            [795, 627, 445, 341],
+            [861, 669, 485, 385],
+            [932, 714, 512, 406],
+            [1006, 782, 568, 442],
+            [1094, 860, 614, 464],
+            [1174, 914, 664, 514],
+            [1276, 1000, 718, 538],
+            [1370, 1062, 754, 596],
+            [1468, 1128, 808, 628],
+            [1531, 1193, 871, 661],
+            [1631, 1267, 911, 701],
+            [1735, 1373, 985, 745],
+            [1843, 1455, 1033, 793],
+            [1955, 1541, 1115, 845],
+            [2071, 1631, 1171, 901],
+            [2191, 1725, 1231, 961],
+            [2306, 1812, 1286, 986],
+            [2434, 1914, 1354, 105],
+            [2566, 1992, 1426, 1096],
+            [2702, 2102, 1502, 1142],
+            [2812, 2216, 1582, 1222],
+            [2956, 2334, 1666, 1276],
+    ];
 
     function getVersion(textLength, errorCorrectionLevel) {            
         for (var version = 1, j = QRErrorCorrectionLevelIndex[errorCorrectionLevel];
-                version < QRCodeLimitLength.length && QRCodeLimitLength[version][j] < textLength;
+                version < QRDataCapacity.length && QRDataCapacity[version][j] < textLength;
                 ++version);
-        if (version >= QRCodeLimitLength.length) {
+        if (version >= QRDataCapacity.length) {
             throw new Error("Too long data, " + textLength + " bytes");
         }
         return version;
